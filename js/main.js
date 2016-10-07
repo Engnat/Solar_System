456d140898668bbcbb3f3b72122876f834cdfe0c
CJC9 <engnatruve@gmail.com>
2016-10-06 21:02:14 -0500

    Movimiento planetario

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+var planets_names = ["earth", "pluton", "futurama", "homero jay simpson",
+"colombia jugando y yo aqui", "namekusei", "death star", "gatito"];//arreglo de nombres de planetas
 var Planet = function(){
   return{
+      //Atributos
       clockwise_translation: Math.round(Math.random()),//movimiento sentido las manecillas=1 contra=0
       size: Math.random() * (100 - 10) + 10,//tamaño random entre 10 y 100 donde el min es 10
-      speed: Math.random() * (10 - 1) + 1,//px->sec velocidad
+      speed: Math.random() * (10 - 1) + 1,//px->sec velocidad deg/seg
       color: "#AAA",
-      orbit_size: Math.random() * (400 - 200) + 200 ,//tamaño de la orbita
+      orbit_size: Math.random() * (300 - 200) + 200 ,//tamaño de la orbita
       orbit_position: Math.random() * 360,//posición del planeta en la orbita
+      name: planets_names.pop(),//esta instruccion toma el último elemento del arreglo y se lo asigna a name
+      dom_orbit: null,//variable que almacena el elemento dom de planet_orbit
+      dom_planet: null,//variable que almacena el elemento dom de planet_body
+      rotation_process_id : null,
+      //Metodos
       getSize : function(){//transforma los valores en pixeles
           return this.size + "px";
       },
@@ -16,20 +24,38 @@ var Planet = function(){
         return this.orbit_size + "px";
       },
       pushDOMElement : function(dom_parent){//creamos los elementos html desde el javascript
-        var planet_li = document.createElement("li");//variable para crear elementos planeta li
-        var planet_span = document.createElement("span");//vartiable para crear elementos planeta span
-        planet_span.setAttribute("class" , "planet");//asignamos la clase planet al elemento planeta span
-        planet_li.appendChild(planet_span);//el elemento span es hijo del elemento li
-        planet_span.style.width = this.getSize();// asignamos ancho al elemento
-        planet_span.style.height = this.getSize();//asignamos alto al elemento
-        planet_li.style.width = this.getOrbitSize();
-        planet_li.style.height = this.getOrbitSize();
-        planet_li.style.position = "relative";//asignamos posicion relativa al elemento
-        planet_li.style.width = this.orbit_size;//
-        planet_span.style.position = "adsolute";
-        planet_span.style.bottom = 0;
-        planet_span.style.right = 0;
-        dom_parent.appendChild(planet_li);
+        var planet_orbit = document.createElement("li");//variable para crear elementos planeta li
+        var planet_body = document.createElement("span");//vartiable para crear elementos planeta span
+        planet_body.setAttribute("class" , "planet");//asignamos la clase planet al elemento planeta span
+        planet_orbit.appendChild(planet_body);//el elemento span es hijo del elemento li
+        planet_body.style.width = this.getSize();// asignamos ancho al elemento
+        planet_body.style.height = this.getSize();//asignamos alto al elemento
+        planet_orbit.style.position = "absolute";//asignamos posicion  al elemento
+        planet_orbit.style.width = this.getOrbitSize();//asigna ancho de orbita
+        planet_orbit.style.height = this.getOrbitSize();//asigna alto de orbita
+        planet_orbit.style.transform = "rotate("+this.orbit_position+"deg)";//rotamos la posicion de los planetas
+        planet_orbit.style.transformOrigin = "top left";//el origen o ancla de rotacion esta en la parte superior izquierda
+        planet_orbit.style.top = 0;
+        planet_orbit.style.left = 0;
+        planet_body.style.position = "absolute";
+        planet_body.style.bottom = 0;
+        planet_body.style.right = 0;
+        planet_body.textContent = this.name;//asigna los nombres a los planetas
+        dom_parent.appendChild(planet_orbit);
+        this.dom_orbit = planet_orbit;//guardo en las variales los elementos dom planet_orbit
+        this.dom_planet = planet_body;//guardo en las variales los elementos dom planet_body
+      },
+      startRotation : function(){
+          var self = this;//guarda el objeto que estoy creando "es planeta porque esta dentro planeta" siempre se define a nivel de mi funcion
+          var freq = 10;
+          self.rotation_process_id = setInterval(function(){//Recibe dos parametros funcion + tiempo ->funcion del lenguaje que ejecuta la funcion indefinidamente que se pase como parametro cada cierto tiempo
+            if(self.clockwise_translation === 1){
+              self.orbit_position += self.speed/freq;//sumamos la velocidad para que corra en sentido
+            }else {
+              self.orbit_position -= self.speed/freq;//restamos la velocidad para que corra en el otro sentido
+            }
+            self.dom_orbit.style.transform = "rotate("+self.orbit_position+"deg)";
+          },500/freq);
       },
   };
 };
@@ -41,8 +67,9 @@ while(number_of_planets-- > 0){
 }
 console.log(planets);
 document.addEventListener("DOMContentLoaded" , function(){
-  var planetary_system = document.querySelector(".planets");
-  for(var planet_pos in planets){
-    planets[planet_pos].pushDOMElement(planetary_system);
+  var planetary_system = document.querySelector(".planets");//creamos planetas
+  for(var planet_pos in planets){//recorre todos los planetas
+    planets[planet_pos].pushDOMElement(planetary_system);//pinta los planetas
+    planets[planet_pos].startRotation();
   }
 });
